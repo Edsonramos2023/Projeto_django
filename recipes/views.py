14c6937a4d847196921581bbc5980c2a2b77aed8
Edson Ramos <storene.contato@gmail.com>
2024-04-01 21:10:35 -0300

    salvar

--- recipes/views.py
+++ recipes/views.py
@@ -1,19 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
        'name': 'Edson Ramos' 
     })
-
-
-def sobre(request):
-    return HttpResponse('sobre')
-
-
-def contato(request):
-    return HttpResponse('contato')
-
-
-
